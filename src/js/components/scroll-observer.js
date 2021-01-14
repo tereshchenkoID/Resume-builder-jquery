0ebc9a470e11723c9298c17d46791c5d3b590187
Oleg Kotsubko <olegkotsubko@gmail.com>
2021-01-14 16:25:56 +0200

    added indicator animation on scroll

--- src/js/components/scroll-observer.js
+++ src/js/components/scroll-observer.js
@@ -4,31 +4,10 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 const scrollAnimation = () => {
-  // const animated = gsap.utils.toArray('.animated')
-
-  // animated.forEach(item => {
-  //   const animationX = gsap.to(item, {
-  //     x: 500,
-  //     rotation: 360,
-  //     duration: 1,
-  //   })
-  //   ScrollTrigger.create({
-  //     trigger: item,
-  //     start: "top 60%",
-  //     toggleActions: "play none none none",
-  //     once: true,
-  //     animation: animationX,
-  //     toggleClass: {
-  //       targets: item,
-  //       className: 'active'
-  //     }
-  //   });
-  // })
 
   ScrollTrigger.batch('.js-accent-background-animate', {
-    start: "top 60%",
+    start: "top 80%",
     toggleClass: "active",
-    // once: true,
   })
 
   ScrollTrigger.batch('.js-image-animation', {
