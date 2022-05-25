f8313480117990b1493bc1e2c3b641b855616f5f
i-tereshchenko <i.tereschenko@weplay.tv>
2022-05-25 21:45:07 +0300

    - add header, navigation

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,19 @@
-
-
 $('.js-toggle').on('click', function() {
   $('.js-toggle').toggleClass('toggle--active')
   $('.js-menu').toggleClass('menu--active')
 })
+
+function getScroll () {
+  if ($(window).scrollTop() > 0) {
+    $('.js-nav').addClass("nav--fixed");
+  }
+  else {
+    $('.js-nav').removeClass("nav--fixed");
+  }
+}
+
+getScroll()
+
+$(window).scroll(() => {
+  getScroll()
+});
