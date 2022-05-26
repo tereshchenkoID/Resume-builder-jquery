1e3247b82d763d44c76cc2c74ce25fdac5b6ecfe
i-tereshchenko <i.tereschenko@weplay.tv>
2022-05-26 15:57:52 +0300

    - contact page

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,3 @@ $('.js-toggle').on('click', function() {
   $('.js-toggle').toggleClass('toggle--active')
   $('.js-menu').toggleClass('menu--active')
 })
-
-function getScroll () {
-  if ($(window).scrollTop() > 0) {
-    $('.js-nav').addClass("nav--fixed");
-  }
-  else {
-    $('.js-nav').removeClass("nav--fixed");
-  }
-}
-
-getScroll()
-
-$(window).scroll(() => {
-  getScroll()
-});
