a24979e4177d5b347df2989f95082153d7974cbc
i-tereshchenko <i.tereschenko@weplay.tv>
2022-05-28 23:26:47 +0300

    - filter cards

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,36 @@
 $('.js-toggle').on('click', function() {
-  $('.js-toggle').toggleClass('toggle--active')
+  $(this).toggleClass('toggle--active')
   $('.js-menu').toggleClass('menu--active')
+})
+
+$('.js-checked').on('click', function() {
+  $(this).toggleClass('checked--active')
+})
+
+$('.js-select').on('click', function(){
+  $(this).toggleClass('select--active');
+})
+
+$('.js-select-item').on('click', function () {
+  $('.js-select-label').text($(this).text());
+  $('.js-select-label').attr('value', $(this).attr('value'));
+})
+
+
+function changeJobsView (value) {
+  if (value ==='list') {
+    $('.js-jobs').removeClass('jobs--grid')
+    $('.js-jobs').addClass('jobs--list')
+  }
+  else {
+    $('.js-jobs').removeClass('jobs--list')
+    $('.js-jobs').addClass('jobs--grid')
+  }
+}
+
+$('.js-actions-item').on('click', function() {
+  $('.js-actions-item').removeClass('actions__item--active')
+  $(this).addClass('actions__item--active')
+
+  changeJobsView(this.getAttribute('data-sort'));
 })
